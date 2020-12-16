300360bd7f92e9a3053fe0dcd39aefba1faad6fa
alemiranda1105 <amiranda110500@gmail.com>
2020-12-16 16:50:33 +0100

    Visualización de imagen terminada

--- src/imageviewer/apps/swing/BlockPanel.java
+++ src/imageviewer/apps/swing/BlockPanel.java
@@ -12,18 +12,20 @@ import java.io.IOException;
 
 public class BlockPanel extends JPanel implements ImageDisplay {
     private Image image;
+    private BufferedImage bufferedImage;
 
     public BlockPanel() {
     }
 
     @Override
     public void paint(Graphics g) {
-        g.drawImage(image(), 0, 0, null);
+        Box box = new Box(bufferedImage);
+        g.drawImage(bufferedImage, box.x, box.y, box.width, box.height, null);
     }
 
-    private BufferedImage image() {
+    private BufferedImage load(String name) {
         try {
-            return ImageIO.read(new File(image.getName()));
+            return ImageIO.read(new File(name));
         } catch (IOException e) {
             System.out.println("Error leyendo la imagen");
             return null;
@@ -33,11 +35,44 @@ public class BlockPanel extends JPanel implements ImageDisplay {
     @Override
     public void display(Image image) {
         this.image = image;
+        this.bufferedImage = load(image.getName());
         repaint();
     }
 
     @Override
     public Image currentImage() {
         return image;
+    }
+
+    private class Box {
+        final int x;
+        final int y;
+        final int width;
+        final int height;
+        private final int px;
+        private final int py;
+
+        public Box(BufferedImage image) {
+            this.px = getWidth();
+            this.py = getHeight();
+            this.width = calculateWidth(image.getWidth(), image.getHeight());
+            this.height = calculateHeight(image.getWidth(), image.getHeight());
+            this.x = (px - width) / 2;
+            this.y = (py - height) / 2;
+        }
+
+        private int calculateWidth(double ix, double iy) {
+            double pr = (double) px / py;
+            double ir = ix / iy;
+            return ir > pr ? px: (int) (ix * py / iy);
+        }
+
+        private int calculateHeight(double ix, double iy) {
+            double pr = (double) px / py;
+            double ir = ix / iy;
+            return ir > pr ? (int) (iy * px / ix): py;
+        }
+
+
     }
 }
